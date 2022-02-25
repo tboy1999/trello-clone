a483d7977c05775996c56d5d017366fac54cb5b0
Tong Tai <meotai1999@gmail.com>
2022-02-25 09:56:05 +0700

    initial data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,18 +1,38 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { isEmpty } from "lodash";
 
 import './BoardContent.scss'
 
 import Column from '../Column/Column'
+import { mapOrder } from "utilities/sorts";
+
+import { initialData } from 'actions/initialData'
 
 function BoardContent() {
-    return (
-      <div className="board-content">
-        <Column />
-        <Column />
-        <Column />
-        <Column />
-      </div>
-    )
+
+  const [board, setBoard] = useState({})
+  const [column, setColumn] = useState([])
+
+  useEffect(() => {
+    const boardFormDB = initialData.boards.find(board => board.id === 'board-1')
+    if (boardFormDB) {
+      setBoard(boardFormDB)
+
+
+      setColumn(mapOrder(boardFormDB.columns, boardFormDB.columnOrder, 'id'))
+    }
+  }, [])
+
+  if (isEmpty(board)) {
+    return <div>Board not found!</div>
+  }
+  return (
+    <div className="board-content">
+      {column.map((column,index) => <Column key={index} column={column}/>)}
+
+      
+    </div>
+  )
 }
 
 export default BoardContent
